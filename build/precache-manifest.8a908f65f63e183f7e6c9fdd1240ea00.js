23038b8a1da5964278c8e28b3d61b0f6c9e8e424
adampi <adam.piskorek.pl@gmail.com>
2020-02-14 22:07:00 +0100

    SImpliest BUILD"

---
+++ build/precache-manifest.8a908f65f63e183f7e6c9fdd1240ea00.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cc573e2ef860965be1ed30f9dda69f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b31ad6c905f1b5f895d",
+    "url": "/static/js/2.44317713.chunk.js"
+  },
+  {
+    "revision": "fdb9b7056a4db8d5fd9e",
+    "url": "/static/js/main.a4ec40b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
