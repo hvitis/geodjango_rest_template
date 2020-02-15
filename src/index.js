c36fbfd813f856ace0252a82bee35740d418b4a1
adampi <adam.piskorek.pl@gmail.com>
2020-02-15 14:28:54 +0100

    Moving JS assets to SRC

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
+
+
+import './assets/vendor_assets/css/bootstrap/bootstrap.css'
 import './assets/style.css';
 import './assets/vendor_assets/css/brands.css'
 import './assets/vendor_assets/css/fontawesome.min.css'
@@ -16,6 +19,21 @@ import './assets/vendor_assets/css/jquery.mCustomScrollbar.css'
 import './assets/vendor_assets/css/line-awesome.min.css'
 import './assets/vendor_assets/css/magnific-popup.css'
 
+
+import './assets/vendor_assets/js/jquery/jquery-1.12.3.js'
+import './assets/vendor_assets/js/bootstrap/popper.js'
+import './assets/vendor_assets/js/bootstrap/bootstrap.min.js'
+import './assets/vendor_assets/js/jquery-ui.min.js'
+import './assets/vendor_assets/js/jquery.barrating.min.js'
+import './assets/vendor_assets/js/jquery.counterup.min.js'
+import './assets/vendor_assets/js/jquery.magnific-popup.min.js'
+import './assets/vendor_assets/js/jquery.mCustomScrollbar.concat.min.js'
+import './assets/vendor_assets/js/jquery.waypoints.min.js'
+import './assets/vendor_assets/js/masonry.pkgd.min.js'    
+import './assets/vendor_assets/js/select2.full.min.js'
+import './assets/vendor_assets/js/slick.min.js'
+import './assets/theme_assets/js/locator.js'
+import './assets/theme_assets/js/main.js'   
 
 import reducer from './Store/Reducers/auth';
 
