4997bcb7e8466028cb938e71f2200f4272c7ff82
Adam Piskorek <adam.piskorek.pl@gmail.com>
2020-02-18 15:22:26 +0100

    Hardcoded PROD

--- src/config.js
+++ src/config.js
@@ -8,11 +8,12 @@ const dev = {
     API_KEY_GMAPS : "AIzaSyA1umOS6BOBvTNLReOage940S6VDqLskkk"
 
   };
-  
+
+  const config = prod;
   // Default to dev if not set
-  const config = process.env.ENVIRONMENT === 'prod'
-    ? prod
-    : dev;
+  // const config = process.env.ENVIRONMENT === 'prod'
+  //   ? prod
+  //   : dev;
   
   export default {
     // Add common config values here
